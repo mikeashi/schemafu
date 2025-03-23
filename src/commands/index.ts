d09b3648e1b713e27e26bba603d947a04e2fe1e1
Mike Ashi <mikeashi2013@gmail.com>
2025-03-23 20:40:44 +0100

    Register validate command in the CLI

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,8 @@
 import { Command } from 'commander'
-import { register as RegisterBundle} from './bundle.js'
+import { register as RegisterBundle } from './bundle.js'
+import { register as RegisterValidate } from './validate.js'
 
 export function registerCommands(program: Command) {
    RegisterBundle(program);
+   RegisterValidate(program);
 }
