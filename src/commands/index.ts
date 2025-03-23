054269759daaa00ffdaf3f18fef78aa3b739ddeb
Mike Ashi <mikeashi2013@gmail.com>
2025-03-23 23:50:12 +0100

    Add process command registration to command index

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,9 +2,11 @@ import { Command } from 'commander'
 import { register as RegisterBundle } from './bundle.js'
 import { register as RegisterValidate } from './validate.js'
 import { register as RegisterGenerate } from './generate.js'
+import { register as RegisterProcess } from './process.js'
 
 export function registerCommands(program: Command) {
    RegisterBundle(program);
    RegisterValidate(program);
    RegisterGenerate(program);
+   RegisterProcess(program);
 }
